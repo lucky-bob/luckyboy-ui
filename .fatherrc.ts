5299dae79ac48fb5c6fb287e61d8c582c6aed1da
chrisbob <caixgwy@163.com>
2023-08-17 23:40:12 +0800

    fix: test

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist/esm-dist' },
-  cjs: { output: 'dist/cjs-dist' },
+  esm: {},
+  cjs: {},
+  umd: {},
 });
